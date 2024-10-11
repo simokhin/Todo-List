ded9b71c5c349be7fe33489fd3517f0b7ac814da
simokhin <nick.simokhin@yandex.ru>
2024-10-12 01:05:10 +0300

    Make function to show task in DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import "./reset.css";
 import "./styles.css";
 
-import { ToDo } from "./todo.js";
+import { ToDo, toDoList, addTask } from "./todo.js";
+
+const task1 = new ToDo("Сделай дело - гуляй смело", "20.12.24", "High");
+addTask(task1);
+dom();
+
+function dom() {
+    const list = document.querySelector(".todolist");
+
+    toDoList.forEach(task => {
+        const div = document.createElement("div");
+        div.classList.add("list-row");
+        div.classList.add("back");
+        list.appendChild(div);
+    
+        const taskName = document.createElement("div");
+        taskName.classList.add("name");
+        taskName.textContent = task.name;
+    
+        const taskDate = document.createElement("div");
+        taskDate.classList.add("date");
+        taskDate.textContent = task.date;
+    
+        const taskPriority = document.createElement("div");
+        taskPriority.classList.add("date");
+        taskPriority.textContent = task.priority;
+    
+        div.append (taskName, taskDate, taskPriority);
+    })
+}
--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,13 @@
+export let toDoList = [];
+
 export class ToDo {
     constructor(name, date, priority) {
         this.name = name;
         this.date = date;
         this.priority = priority;
     }
+}
+
+export function addTask(task) {
+    toDoList.push(task);
 }
